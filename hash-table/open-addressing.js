e8591f29dd1cfcc3a02d935ae0d516878f7f4780
nvdai2401 <nvdai2401@gmail.com>
2020-06-02 15:04:26 +0700

    implemented hash table using open addressing technique

---
+++ hash-table/open-addressing.js
@@ -0,0 +1,147 @@
+const DEFAULT_CAPACITY = 8
+const UPPER_LOAD_FACTOR = 0.7
+const LOWER_LOAD_FACTOR = 0.25
+const GROW_SHRINK_FACTOR = 2
+
+class Node {
+  constructor(key, value, deleted = false) {
+    this.key = key
+    this.value = value
+    this.deleted = deleted
+  }
+}
+
+class OpenAddressingHT {
+  constructor() {
+    this.arr = new Array(DEFAULT_CAPACITY)
+    this.capacity = DEFAULT_CAPACITY
+    this.size = 0
+  }
+
+  insert(key, value) {
+    const { curNode, index } = this.__search(key)
+
+    if (!curNode || curNode.deleted) {
+      const node = new Node(key, value)
+      this.arr[index] = node
+      this.size++
+      if (this.__getGrowShrinkFactor() >= UPPER_LOAD_FACTOR) {
+        this.__grow()
+      }
+      return
+    } 
+
+    if (curNode.key === key) {
+      this.arr[index].value = value
+      return
+    }
+  }
+
+  search(key) {
+    const { curNode } = this.__search(key)
+    return curNode ? curNode.value : curNode
+  }
+
+  delete(key) {
+    const { curNode, index } = this.__search(key)
+    if (!curNode) return 
+  
+    this.arr[index].deleted = true
+    this.size--
+    if (this.__getGrowShrinkFactor() <= LOWER_LOAD_FACTOR) {
+      this.__shrink()
+    }
+  }
+
+  __search(key) {
+    let trial = 1
+
+    while (trial <= this.capacity) {
+      const index = this.__doubleHash(key, trial)
+      const curNode = this.arr[index]
+
+      if (!curNode) return { curNode: null, index }
+
+      if (curNode.key === key && !curNode.deleted) {
+        return { curNode, index }
+      }
+      trial++
+    }
+  }
+
+  __grow() {
+    const newCapacity = this.capacity * GROW_SHRINK_FACTOR
+    this.__rehash(newCapacity)
+  }
+
+  __shrink() {
+    const newCapacity = this.capacity / GROW_SHRINK_FACTOR
+    this.__rehash(newCapacity)
+  }
+
+  __rehash(newCapacity) {
+    const oldCapacity = this.capacity
+    const oldArr = [...this.arr]
+    
+    this.capacity = newCapacity
+    this.arr = new Array(this.capacity)
+    this.size = 0
+
+    for (let i = 0; i < oldCapacity; i++) {
+      if (oldArr[i] && oldArr[i].deleted === false) {
+        this.insert(oldArr[i].key, oldArr[i].value)
+      }
+    }
+  }
+
+  __getGrowShrinkFactor() {
+    return this.size / this.capacity
+  }
+  
+  __doubleHash(k, trial) {
+    return (this.__hash1(k) + trial * this.__hash2(k)) % this.capacity
+  }
+
+  __hash1(key) {
+    return key % this.capacity
+  }
+
+  __hash2(key) {
+    return this.__nearestPrime(this.capacity) - (key % this.__nearestPrime(this.capacity))
+  }
+
+  __nearestPrime(k) {
+    for (let i = k - 1; i >= 2; i--) {
+      if (this.__isPrime(i)) return i
+    }
+  }
+
+  __isPrime(k) {
+    if (k <= 1) return false
+    for (let i = 2; i <= Math.trunc(k / 2); i++) {
+      if (k % i === 0) return false
+    }
+    return true
+  }
+  
+}
+
+const HT = new OpenAddressingHT()
+
+HT.insert(1, 1002)
+HT.insert(2, 101)
+HT.insert(3, 101)
+HT.insert(4, 101)
+HT.insert(5, 101)
+HT.insert(6, 101)
+HT.insert(7, 101)
+// HT.insert(18, 1021)
+// HT.delete(5)
+// HT.delete(6)
+// HT.delete(7)
+// HT.delete(1)
+// HT.delete(2)
+console.log(HT)
+console.log(HT.search(4))
+
+module.exports = { OpenAddressingHT }
