edf6ecc5c85a816375d2eba5619641697ab4faa9
nvdai2401 <nvdai2401@gmail.com>
2020-06-09 12:50:48 +0700

    implement insert and delete method for AVL tree

--- tree/avl-tree/avl-tree.js
+++ tree/avl-tree/avl-tree.js
@@ -11,32 +11,58 @@ class AVLTree extends BinarySearchTree {
     }
   }
 
-  balance(node) {
-    // if (node.balanceFactor < -1) {
-    //   if (node.right === null) {
-    //     this.rightRotate(node.right)
-    //   }
-    //   this.leftRotate(node)
-    // }
+  delete(value) {
+    super.delete(value)
+    this.balance(this.root)
+  }
+
+  balance(node) { 
+    if (node.balanceFactor < -1) {
+      if (node.right.right === null) {
+        this.rightRotate(node.right)
+      }
+      this.leftRotate(node)
+    }
 
     if (node.balanceFactor > 1) {
-      if (node.left === null) {
+      console.log(node.value)
+      if (node.left.left === null) {
         this.leftRotate(node.left)
       }
       this.rightRotate(node)
     }
   }
+
+  leftRotate(rootNode) {
+    const rightNode = rootNode.right
+    rootNode.setRight(null)
+    if (rootNode.parent === null) {
+      rightNode.parent = null
+      this.root = rightNode
+    } else {
+      rootNode.parent.setLeft(rightNode)
+    }
+    
+    rightNode.setLeft(rootNode)
+  }
   
   rightRotate(rootNode) {
     const leftNode = rootNode.left
     rootNode.setLeft(null)
-    
+
     if (rootNode.parent === null) {
+      leftNode.parent = null
       this.root = leftNode
     } else {
-      rootNode.parent.setLeft(leftNode)
-    }
+      if (rootNode.parent.left && rootNode.parent.left.value === rootNode.value) {
+        rootNode.parent.setLeft(leftNode)
+      }
 
+      if (rootNode.parent.right && rootNode.parent.right.value === rootNode.value) {
+        rootNode.parent.setRight(leftNode)
+      }
+    }
+    
     leftNode.setRight(rootNode)
   }
 
@@ -49,8 +75,8 @@ AVL.insert(18)
 AVL.insert(11)
 AVL.insert(17)
 AVL.insert(5)
-AVL.insert(4.5)
-
+AVL.insert(4)
 AVL.insert(3)
+AVL.insert(6)
 
-console.log(AVL.root)
+console.log(AVL.root.traverseInOrder())
